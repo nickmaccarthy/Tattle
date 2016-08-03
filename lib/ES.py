a5f6c5996c24decd1bb7b9311316477b567866e7
Nick M <nickmaccarthy@gmail.com>
2016-08-03 13:11:53 -0400

    new config methodology

--- lib/ES.py
+++ lib/ES.py
@@ -12,12 +12,12 @@ class ESConnectException(Exception):
     underneath the covers, the Elasticsearch class will maintain state for us and keep us thread safe
 '''
 def connect():
-    conf = tattle.config.load_tattle_config()
+    conf = tattle.config.load_configs().get('elasticsearch')
     try:
-        es = Elasticsearch(conf['Elasticsearch']['servers'], **conf['Elasticsearch']['args'])
+        es = Elasticsearch(conf['servers'], **conf['args'])
         return es
     except Exception as e:
-        err_msg = "Unable to establish connection to the Elasticsearch cluster. Config: {}, error: {}".format(conf, error)
+        err_msg = "Unable to establish connection to the Elasticsearch cluster. Config: {}, error: {}".format(conf, e)
         logger.exception(err_msg)
         raise ESConnectException(err_msg)
         
